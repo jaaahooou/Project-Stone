cba0a1761aae2f7556b3ced39d56cc5caf514e76
Jacek Pilch <jaaahooou@wp.pl>
2021-05-08 12:30:28 +0200

    Media for 1600px and problems with navigations

--- js/menu_locations.js
+++ js/menu_locations.js
@@ -7,6 +7,14 @@ const aboutInspirations = document.querySelector('.inspirations_button');
 const introWrapper = document.querySelector('.intro_wrapper');
 
 
+
+
+
+
+
+
+
+
 logoButton.addEventListener('click', () => {
     location.href = './index.html';
 
@@ -28,6 +36,7 @@ aboutStonesButton.addEventListener('click', () => {
 })
 
 aboutLargeFormatTilesButton.addEventListener('click', ()=>{
+    console.log('działam');
     location.href = './about_large_format_tiles.html'
 })
 
@@ -35,9 +44,47 @@ aboutLargeFormatTilesButton.addEventListener('click', ()=>{
 
 // --------  MOBILE MENU --------
 
+
+const aboutUsButtonMobile = document.querySelector('.about_us_button_mobile');
+const aboutTilesButtonMobile = document.querySelector('.about_tiles_button_mobile');
+const logoButtonMobile = document.querySelector('.logo_mobile');
+const aboutStonesButtonMobile = document.querySelector('.about_stones_button_mobile');
+const aboutLargeFormatTilesButtonMobile = document.querySelector('.about_lagre_format_tiles_button_mobile');
+const aboutInspirationsMobile = document.querySelector('.inspirations_button_mobile');
+const introWrapperMobile = document.querySelector('.intro_wrapper');
+
+
+
+logoButtonMobile.addEventListener('click', () => {
+    location.href = './index.html';
+
+})
+
+aboutUsButtonMobile.addEventListener('click', () => {
+    location.href = './about_us.html'
+    previousUrl()
+})
+
+aboutTilesButtonMobile.addEventListener('click', () => {
+    location.href = './about_tiles.html'
+    introWrapper.classList.add('hidden')
+})
+
+aboutStonesButtonMobile.addEventListener('click', () => {
+    location.href = './about_stones.html'
+
+})
+
+aboutLargeFormatTilesButtonMobile.addEventListener('click', ()=>{
+    console.log('działam');
+    location.href = './about_large_format_tiles.html'
+})
+
+
 const mobileNavigation = document.querySelector('.mobile-navigation')
 const hamburgerButton = document.querySelector('.fa-bars');
 const articleWrapper = document.querySelector('.article_wrapper');
+const mainPageContentWrapper = document.querySelector('.main_page_content_wrapper');
 const mobileNavigationContentWrapper = document.querySelector('.mobile-navigation-content-wrapper') ;
 const socialMediaWrapperMobile = document.querySelector('.social-media-buttons-for-mobile-navigation');
 const instagramButtonMobile = document.querySelector('.instagram-button-mobile');
@@ -80,10 +127,24 @@ hamburgerButton.addEventListener('click',()=>{
    
 })
 
-articleWrapper.addEventListener('click',()=>{
-if (mobileNavigation.classList.contains('mobile-menu-is-active')){
-    closeMobileMenu();
+
+if(articleWrapper){
+    articleWrapper.addEventListener('click',()=>{
+        if (mobileNavigation.classList.contains('mobile-menu-is-active')){
+            closeMobileMenu();
+        }
+            
+        }
+        )
 }
-    
+
+if(mainPageContentWrapper){
+    mainPageContentWrapper.addEventListener('click',(event)=>{
+        if (mobileNavigation.classList.contains('mobile-menu-is-active')){
+            closeMobileMenu();
+            event.preventDefault()
+        }
+            
+        }
+        )
 }
-)
