4044a663b13ca057362012605b4d8dde8444559c
Jacek Pilch <jaaahooou@wp.pl>
2021-05-21 08:21:35 +0200

    scroll

--- js/about_us/main.js
+++ js/about_us/main.js
@@ -3,7 +3,7 @@ const downArrowMobile = document.querySelector('.down-arrow-mobile')
 
 window.addEventListener('scroll', ()=>{
 
-    let opacity = window.pageYOffset *2.5 /1000
+    let opacity = window.pageYOffset *2 /1000
     
     downArrowMobile.style.opacity = 1 - `${opacity}`;
     console.log(opacity);
