3c04e79a8101774ae2693602fa899d9fc8d28d59
Jacek Pilch <jaaahooou@wp.pl>
2021-05-24 22:01:27 +0200

    styling animations 'about us'

--- js/about_us/main.js
+++ js/about_us/main.js
@@ -1,10 +1,38 @@
-const downArrowMobile = document.querySelector('.down-arrow-mobile')
+const whatWeDo = document.querySelector('.what-we-do');
+const whatWeDoSecond = document.querySelector('.what-we-do-second');
+const whatWeDoThird = document.querySelector('.what-we-do-third');
+const introduce = document.querySelector('.introduce');
+const logo = document.querySelector('.logo-colour');
+
+let opacity = 0;
+let opacitySecond = 0;
+let opacityThird = 0;
 
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', (e)=>{
 
-    let opacity = window.pageYOffset *2 /1000
-    
-    downArrowMobile.style.opacity = 1 - `${opacity}`;
-    console.log(opacity);
+       console.log(window.pageYOffset);
+
+if(window.pageYOffset >= 200){
+   whatWeDo.classList.add('slide-element-from-left')
+}    
+
+if(window.pageYOffset >= 600){
+    whatWeDoSecond.classList.add('slide-element-from-right')
+}
+if(window.pageYOffset >= 1000){
+   whatWeDoThird.classList.add('show-element')
+}
+
 })
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    introduce.classList.add('slide-element-from-right');
+
+    console.log(logo);
+    logo.classList.add('slide-element-from-left');
+
+})
+
+
