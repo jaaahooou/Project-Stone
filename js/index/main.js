301266725e0ce36eef6ce7baedcf8523bd8c3d43
Jacek Pilch <jaaahooou@wp.pl>
2021-06-08 09:13:22 +0200

    Put innerText to tiles descriptions

--- js/index/main.js
+++ js/index/main.js
@@ -1,19 +1,20 @@
 let itemBackground = [...document.querySelectorAll('.item_background')];
 let white = [...document.querySelectorAll(".white")];
 const list = document.querySelectorAll('#list li');
-const tilesDescriptions = [...document.querySelectorAll('.item_description')];
+const tilesDescriptions = document.querySelectorAll('.item_1_descritpion h1');
+
 
 
 
 const descriptions = [
-        { id: 1, descrition : "cuarzo-reno", url: '../../img/main-site/small_images/cuarzo-reno.jpg'},
-        { id: 2, descrition : "loire-arena", url: '../../img/main-site/small_images/loire-arena.jpg'},
-        { id: 3, descrition : "artic-antracita", url: '../../img/main-site/small_images/artic-antracita.jpg'},
-        { id: 4, descrition : "gala-penelope", url: '../../img/main-site/small_images/gala-penelope.jpg'},
-        { id: 5, descrition : "ragno-look", url: '../../img/main-site/small_images/ragno-look.jpg'},
-        { id: 6, descrition : "patagonia-castano", url: '../../img/main-site/small_images/patagonia-castano.jpg'},
-        { id: 7, descrition : "abadia-fuego", url: '../../img/main-site/small_images/abadia-fuego.jpg'},
-        { id: 8, descrition : "volcano-corten", url: '../../img/main-site/small_images/volcano-corten.jpg'},
+        { id: 1, description : "cuarzo reno", url: '../../img/main-site/small_images/cuarzo-reno.jpg'},
+        { id: 2, description : "loire arena", url: '../../img/main-site/small_images/loire-arena.jpg'},
+        { id: 3, description : "artic antracita", url: '../../img/main-site/small_images/artic-antracita.jpg'},
+        { id: 4, description : "gala penelope", url: '../../img/main-site/small_images/gala-penelope.jpg'},
+        { id: 5, description : "ragno look", url: '../../img/main-site/small_images/ragno-look.jpg'},
+        { id: 6, description : "patagonia castano", url: '../../img/main-site/small_images/patagonia-castano.jpg'},
+        { id: 7, description : "abadia fuego", url: '../../img/main-site/small_images/abadia-fuego.jpg'},
+        { id: 8, description : "volcano corten", url: '../../img/main-site/small_images/volcano-corten.jpg'},
 ]
 
 
@@ -26,8 +27,13 @@ const descriptions = [
 
 
 const createDescritionsOfTiles = (i) =>{
-    console.log(i);
-    console.log(typeof tilesDescriptions[i]);
+   
+
+    tilesDescriptions[i].innerText = `${descriptions[i].description}`;
+    
+    
+
+   
 
 }
 
@@ -38,7 +44,7 @@ for (let i = 0; i < list.length; i++) {
     createDescritionsOfTiles(i);
 
     list[i].addEventListener('click', () => {
-        console.log(list[i]);
+       
         
         mainInspirationsForMobilePhones(i);
 
@@ -73,10 +79,7 @@ for (let i = 0; i < list.length; i++) {
 
 const mainInspirationsForMobilePhones = (i) =>{
     if(window.innerWidth < 1023){
-        
-
-
-        console.log(descriptions[i].url);
+                console.log(descriptions[i].url);
     
     }
     
