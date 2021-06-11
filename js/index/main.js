f3fd1995a5e4d5c2ebc4a6395d4c74fb644a8433
Jacek Pilch <jaaahooou@wp.pl>
2021-06-11 21:34:33 +0200

    Fix problems with small pics on main site

--- js/index/main.js
+++ js/index/main.js
@@ -10,14 +10,14 @@ const productList = document.querySelector('.product_list');
 
 
 const descriptions = [
-        { id: 1, description : "cuarzo reno", url: '../../img/main-site/small_images/cuarzo-reno.jpg'},
-        { id: 2, description : "loire arena", url: '../../img/main-site/small_images/loire-arena.jpg'},
-        { id: 3, description : "artic antracita", url: '../../img/main-site/small_images/artic-antracita.jpg'},
-        { id: 4, description : "gala penelope", url: '../../img/main-site/small_images/gala-penelope.jpg'},
-        { id: 5, description : "ragno look", url: '../../img/main-site/small_images/ragno-look.jpg'},
-        { id: 6, description : "patagonia castano", url: '../../img/main-site/small_images/patagonia-castano.jpg'},
-        { id: 7, description : "abadia fuego", url: '../../img/main-site/small_images/abadia-fuego.jpg'},
-        { id: 8, description : "volcano corten", url: '../../img/main-site/small_images/volcano-corten.jpg'},
+        { id: 1, description : "cuarzo reno", url: './img/main-site/small_images/cuarzo-reno.jpg'},
+        { id: 2, description : "loire arena", url: './img/main-site/small_images/loire-arena.jpg'},
+        { id: 3, description : "artic antracita", url: './img/main-site/small_images/artic-antracita.jpg'},
+        { id: 4, description : "gala penelope", url: './img/main-site/small_images/gala-penelope.jpg'},
+        { id: 5, description : "ragno look", url: './img/main-site/small_images/ragno-look.jpg'},
+        { id: 6, description : "patagonia castano", url: './img/main-site/small_images/patagonia-castano.jpg'},
+        { id: 7, description : "abadia fuego", url: './img/main-site/small_images/abadia-fuego.jpg'},
+        { id: 8, description : "volcano corten", url: './img/main-site/small_images/volcano-corten.jpg'},
 ]
 
 
